7ed59e7a62414ee019d0e019da1d3162ace2ba7a
Arturo Vargas Cuevas <a01652564@tec.mx>
2023-03-08 15:52:04 -0600

    feat: add R-peak detection with self-adjustable thresholds
      Implement adaptive R-peak detection measuring absolute maximum during 5000-sample
      adaptation phase. Calculate R-peak threshold dynamically at 60% of observed max.
      Detect and trigger on R-peaks post-adaptation using conditioned signal range
      (0.61V-4.47V).

--- src/sketch.ino
+++ src/sketch.ino
@@ -1,18 +1,18 @@
 /**
  * @file    sketch.ino
- * @brief   ECG Gating System - Basic Threshold Detection (Phase 4 - V1)
+ * @brief   ECG Gating System - R-Peak Detection with Self-Adjustable Thresholds
  *
- * First production version implementing basic ECG threshold detection.
- * Acquires raw ECG signal and compares against fixed voltage threshold (1.46V).
- * Outputs digital HIGH when signal exceeds threshold, LOW otherwise.
- * Simple finite state logic without adaptation or peak detection.
+ * Implements adaptive R-peak detection using signal statistics. System measures
+ * absolute maximum value during initial adaptation phase (5000 samples), then
+ * calculates R-peak threshold as percentage of observed maximum. Detects R-peaks
+ * by comparing signal samples against dynamically computed threshold.
  *
  * @note    Arduino hardware: ADC input range 0-1023 bits maps to 0-5V physical input.
- *          Uses standard 5V reference for ADC conversion (not conditioned circuit).
- *          Digital output on Pin 6 triggers when threshold exceeded.
+ *          ECG signal conditioning circuit constrains output to 0.61V-4.47V range.
+ *          Digital output on Pin 11 fires when R-peak detected.
  *
  * @author  Arturo Vargas Cuevas (A01652564)
- * @date    2023-03-06
+ * @date    2023-03-08
  */
 
 /* ============================================================================
@@ -21,21 +21,24 @@
  */
 
 /* Arduino analog input pin for ECG signal acquisition */
-const int adc_pin = A0;
+const int adc_pin = A3;
 
-/* Arduino digital output pin for threshold trigger */
-const int output_pin = 6;
+/* Arduino digital output pin for R-peak trigger */
+const int output_pin = 11;
 
 /* ============================================================================
- * Threshold Detection Variables
+ * ADC Parametrization Constants
  * ============================================================================
  */
 
-/* Fixed voltage threshold for ECG detection (V) */
-const float threshold_voltage = 1.46;
+/* Minimum valid ECG input voltage from conditioning circuit (V) */
+const float adc_min_voltage = 0.610;
 
-/* Threshold voltage converted to ADC bit value (0-1023) */
-int threshold_bit;
+/* Maximum valid ECG input voltage from conditioning circuit (V) */
+const float adc_max_voltage = 4.470;
+
+/* Total voltage span of the input signal (V) */
+float adc_voltage_range;
 
 /* ============================================================================
  * Real-Time Signal Processing State
@@ -48,35 +51,108 @@ int ecg_sample;
 /* Current digital output state (HIGH/LOW) */
 int output_state = LOW;
 
+/* Loop iteration counter for adaptation phase tracking */
+int loop_count = 0;
+
+/* Flag indicating adaptation phase completion (5000 samples collected) */
+boolean is_adaptation_complete = false;
+
+/* ============================================================================
+ * Peak Detection Algorithm Variables
+ * ============================================================================
+ */
+
+/* Running maximum value during current detection window */
+int max_value = 0;
+
+/* Latest confirmed absolute maximum (R-peak candidate) */
+int absolute_max = 0;
+
+/* Previous maximum value for edge detection */
+int previous_max = 0;
+
+/* Flag indicating new absolute maximum was found */
+int absolute_max_flag = 0;
+
+/* ============================================================================
+ * R-Wave (QRS Complex) Detection Variables
+ * ============================================================================
+ */
+
+/* Detected R-peak (absolute maximum) value during adaptation */
+int r_max = 0;
+
+/* R-peak threshold as percentage of absolute maximum */
+const float r_peak_percentage = 0.6;
+
+/* Computed R-peak threshold value for firing */
+int r_threshold = 0;
+
 
 void setup()
 {
     /* Configure digital output pin as active-high trigger */
     pinMode(output_pin, OUTPUT);
 
-    /* Convert threshold voltage to ADC bit value using standard 5V reference.
-     * Scaling formula: bit_value = (voltage_threshold / 5.0) * 1023
-     * ADC has 10-bit resolution (0-1023 levels for 0-5V range).
+    /* Compute ADC voltage-to-bit scaling factor.
+     * Signal conditioning circuit constrains all ECG samples to [adc_min_voltage,
+     * adc_max_voltage] range. This allows direct threshold comparison on ADC samples.
      */
-    threshold_bit = round(threshold_voltage / (5.0 / 1023.0));
+    adc_voltage_range = adc_max_voltage - adc_min_voltage;
 }
 
 
 void loop()
 {
-    /* Read fresh ECG sample from analog input */
+    /* Acquire fresh ECG sample from analog conditioning circuit */
     ecg_sample = analogRead(adc_pin);
 
-    /* Simple threshold comparison */
-    if (ecg_sample >= threshold_bit) {
-        output_state = HIGH;
+    /* Loop counter for adaptation phase tracking */
+    if (loop_count < 5000) {
+        loop_count++;
+    } else {
+        is_adaptation_complete = true;
     }
 
-    if (ecg_sample < threshold_bit) {
-        output_state = LOW;
+    /* During adaptation phase, determine R-peak threshold from signal maximum */
+    if (!is_adaptation_complete) {
+        r_max = find_absolute_max(ecg_sample);
+        r_threshold = round(r_max * r_peak_percentage);
     }
 
-    /* Write output state to digital pin */
-    digitalWrite(output_pin, output_state);
+    /* After adaptation phase, fire output when R-peak detected */
+    if (is_adaptation_complete) {
+        if (ecg_sample > r_threshold) {
+            digitalWrite(output_pin, HIGH);
+        } else {
+            digitalWrite(output_pin, LOW);
+        }
+    }
+}
+
+/* ============================================================================
+ * Helper Functions
+ * ============================================================================
+ */
+
+/* Find absolute maximum value in the input signal.
+ * Tracks the maximum value and updates absolute maximum when a new peak is found.
+ * Used during adaptation phase to calibrate R-peak threshold.
+ */
+int find_absolute_max(int ecg_sample)
+{
+    /* Track running maximum: update if new sample exceeds current max */
+    if (ecg_sample > max_value) {
+        max_value = ecg_sample;
+        previous_max = max_value;
+    }
+
+    /* Update absolute maximum when running max changes */
+    if (max_value != absolute_max) {
+        absolute_max = max_value;
+        absolute_max_flag = 1;
+    }
+
+    return absolute_max;
 }
 
